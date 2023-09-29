d51a1396323614d7050e5880a24209321f686c2e
Ariel Carvajal Boyd <arie.cbpro@gmail.com>
2023-09-28 22:00:44 -0300

    Add solution

---
+++ hackerrank/challenges/lonely_integer.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/lonely-integer/problem
+
+import math
+import os
+import random
+import re
+import sys
+from functools import reduce
+from operator import xor
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    return reduce(xor, a)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
